2e560ffa46bc12681b68ba4720d2ef7fd978a6c9
Lisa Schoofs <lisaschoofs@gmail.com>
2017-07-06 18:51:47 -0500

    adds time capture for greeting variable

--- scripts/factories/DashboardService.js
+++ scripts/factories/DashboardService.js
@@ -1,32 +1,49 @@
 myApp.factory('DashboardService', [function(){
-  console.log('DashboardService loaded!');
-  console.log('logging guests', GUESTS);
-  console.log('logging companies', COMPANIES);
-  console.log('logging MESSAGE_TEMPLATES', MESSAGE_TEMPLATES);
 
   let newTemplates = {
     data: []
     }
 
-  console.log('logging MESSAGE_TEMPLATES pre function', MESSAGE_TEMPLATES);
   changeTemplates(MESSAGE_TEMPLATES);
-  console.log('logging new templates', newTemplates);
 
-
+/*takes in each template and replaces each variable, currently with a hard-coded
+value. Eventually will bring in the appropriate company, guest, etc. */
   function changeTemplates(templates) {
     for(var i = 0; i < templates.length; i++){
-      var currentTemplate = templates[i];
-      console.log('currentTemplate: ', currentTemplate);
-      console.log('logging templates in loop', templates);
-      console.log('logging templates[i] in function', templates[i]);
-      console.log('logging templates.length', templates.length);
-      let newTemplate = currentTemplate.message.replace(/greeting/i, 'HI THERE');
+      let newTemplate = templates[i].message.replace(/greeting/i, 'HI THERE');
       let newerTemplate = newTemplate.replace(/firstName/i, 'Lisa');
       let newestTemplate = newerTemplate.replace(/company/i, 'The Hilton');
       let lastTemplate = newestTemplate.replace(/roomNumber/i, 'Room 123');
       newTemplates.data.push(lastTemplate);
     }
   }
+
+//GRABS CURRENT TIME TO INFORM GREETING VARIABLE
+  let date = new Date();
+  let time = date.getHours();
+  console.log('logging time: ', time);
+
+//Variables to inform when each type of greeting should be used  
+  let morningGreetingMax = 12;
+  let afternoonGreetingMin = 12;
+  let afternoonGreetingMax = 17;
+  let eveningGreetingMin = 17;
+  let greeting;
+
+  function checkTime() {
+    if(time < morningGreetingMax) {
+      greeting = 'Good Morning';
+    } else if (time > afternoonGreetingMin && time < afternoonGreetingMax) {
+      greeting = 'Good Afternoon';
+    } else if (time > eveningGreetingMin) {
+      greeting = 'Good Evening';
+    }
+    return greeting;
+  };
+
+  console.log(checkTime());
+  console.log('greeting: ', greeting);
+//
 
   return {
     newTemplates
